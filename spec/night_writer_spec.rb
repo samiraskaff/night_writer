f099e07bba173b01c4feaa6ea9b987b52913c1e6
Sage Skaff <skaffsage@gmail.com>
2022-04-22 16:51:02 -0700

    Add instantiation test

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,5 +1,9 @@
 require './spec/spec_helper'
 
 RSpec.describe NightWriter do
-  
+  let(:nightwriter) { NightWriter.new(input_file) }
+
+  it "exists" do
+    expect(nightwriter).to be_a(NightWriter)
+  end
 end
