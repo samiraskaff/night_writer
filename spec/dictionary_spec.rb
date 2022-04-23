c6491fe141257c0d6b77eca2cb8581f7c52e4695
Sage Skaff <skaffsage@gmail.com>
2022-04-23 15:52:08 -0700

    Add instantiation tests

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -6,5 +6,13 @@ require './lib/night_writer'
 require "./lib/dictionary"
 
 RSpec.describe Dictionary do
-  
+  let(:dictionary) {Dictionary.new}
+
+  it "exists" do
+    expect(dictionary).to be_a(Dictionary)
+  end
+
+  it "has readable attributes" do
+    expect(dictionary.two_by_three_characters["a"]).to eq(["0.", "..", ".."])
+  end
 end
