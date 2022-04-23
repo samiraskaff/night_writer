42a546b444205a8d54c711b8c9456271a4c13ec2
Sage Skaff <skaffsage@gmail.com>
2022-04-23 12:11:07 -0700

    Update spec; delete spec_helper

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,9 +1,17 @@
-require './spec/spec_helper'
+require 'simplecov'
+SimpleCov.start
+
+require "rspec"
+require './lib/night_writer'
 
 RSpec.describe NightWriter do
   let(:nightwriter) { NightWriter.new }
 
   it "exists" do
     expect(nightwriter).to be_a(NightWriter)
+  end
+
+  it "prints a message" do
+    expect(nightwriter.start).to eq("Created 'braille.txt' containing 256 characters")
   end
 end
--- spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-require 'simplecov'
-SimpleCov.start
-
-require "rspec"
-require './lib/night_writer'
