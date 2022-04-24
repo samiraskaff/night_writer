4342158a8def011acb776c47d8a72913f864bebe
Sage Skaff <skaffsage@gmail.com>
2022-04-24 10:29:00 -0700

    Add format translation method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,9 +1,12 @@
+require "./lib/dictionary"
+
 class NightWriter
-  attr_reader :output_file, :input_file, :output_file_name
+  attr_reader :output_file, :input_file, :output_file_name, :dictionary
 
   def initialize(input, output)
     @input_file = File.read(input)
     @output_file_name = output
+    @dictionary = Dictionary.new
   end
 
   def creation_message
@@ -14,10 +17,26 @@ class NightWriter
     input_file.length
   end
 
-  def to_braille
-    @output_file = File.write(output, input_file.reverse)
+  def write_to_braille
+    @output_file = File.write(output, format_translation(dictionary.translate(input_file)))
   end
-  
+
+  def format_translation(input_translation)
+    output_string = ""
+    input_translation.each do |character_array|
+      output_string += character_array[0]
+    end
+    output_string += "\n"
+    input_translation.each do |character_array|
+      output_string += character_array[1]
+    end
+    output_string += "\n"
+    input_translation.each do |character_array|
+      output_string += character_array[2]
+    end
+    output_string += "\n"
+    output_string
+  end
 end
 
 # ifile = ARGV[0]
