db4b9a7cbe0a95167c5e74b93c0a078a2ffad7ec
Sage Skaff <skaffsage@gmail.com>
2022-04-24 16:17:16 -0700

    Add instantiation test

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -5,5 +5,10 @@ require "rspec"
 require "./lib/night_reader"
 
 RSpec.describe NightReader do
+  let(:nightreader) {NightWriter.new('spec_braille.txt', 'spec_original.txt')}
+
+  it "exists" do
+    expect(nightreader).to be_a(NightReader)
+  end
   
 end
