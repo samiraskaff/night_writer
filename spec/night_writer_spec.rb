0d9c093d1a4be000d5a79e457007bf30187e875a
Sage Skaff <skaffsage@gmail.com>
2022-04-24 08:46:11 -0700

    Add format translation test

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -5,14 +5,14 @@ require "rspec"
 require './lib/night_writer'
 
 RSpec.describe NightWriter do
-  let(:nightwriter) {NightWriter.new('spec_message.txt', 'output_message.txt')}
+  let(:nightwriter) {NightWriter.new('spec_message.txt', 'spec_braille.txt')}
 
   it "exists" do
     expect(nightwriter).to be_a(NightWriter)
   end
 
   xit "prints a message based on user input" do
-    expect(nightwriter.creation_message).to eq("Created 'output_message.txt' containing 23 characters")
+    expect(nightwriter.creation_message).to eq("Created 'spec_braille.txt' containing 23 characters")
   end
 
   xit "can return the amount of characters from the created file" do
@@ -20,6 +20,12 @@ RSpec.describe NightWriter do
   end
 
   xit "can convert english to braille" do
-    expect(nightwriter.to_braille).to eq()
+    expect(nightwriter.write_to_braille).to eq()
+  end
+
+  it "can format translations" do
+    expect(nightwriter.format_translation(
+      [["0.", "..", ".."], ["0.", "0.", ".."]])
+    ).to eq("0.0.\n..0.\n....\n")
   end
 end
