1a9ad3f7d02aed4c614c2d4b1afffae8e12d1971
Sage Skaff <skaffsage@gmail.com>
2022-04-25 10:22:23 -0700

    Add format_braille test

--- spec/braille_dictionary_spec.rb
+++ spec/braille_dictionary_spec.rb
@@ -11,4 +11,12 @@ RSpec.describe BrailleDictionary do
   it "exists" do
     expect(dictionary).to be_a(BrailleDictionary)
   end
+
+  it "has readable attributes" do
+    expect(dictionary.alphabet["0....."]).to eq("a")
+  end
+
+  it "can format braille characters" do
+    expect(dictionary.format_braille("0.0.\n..0.\n....\n")).to eq(["0.....", "0.0..."])
+  end
 end
