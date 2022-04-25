2d2c69ab7ec29516000706b632f57af4a225ac50
Sage Skaff <skaffsage@gmail.com>
2022-04-25 09:26:54 -0700

    Template from NightWriter

---
+++ lib/night_reader.rb
@@ -0,0 +1,29 @@
+require "./lib/dictionary"
+
+class NightReader
+  attr_reader :output_file, :input_file, :output_file_name, :dictionary
+
+  def initialize(input, output)
+    @input_file = File.read(input)
+    @output_file_name = output
+    @dictionary = Dictionary.new
+  end
+
+  def creation_message
+    p "Created '#{output_file_name}' containing #{character_count} characters"
+  end
+
+  def character_count
+    input_file.delete("\n").length / 6
+  end
+
+  def english_string
+    dictionary.format_translation(dictionary.translate(input_file))
+  end
+
+  def write_to_english
+    @output_file = File.write(output_file_name, english_string)
+    creation_message
+  end
+
+end
--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -5,10 +5,19 @@ require "rspec"
 require "./lib/night_reader"
 
 RSpec.describe NightReader do
-  let(:nightreader) {NightWriter.new('spec_braille.txt', 'spec_original.txt')}
+  let(:nightreader) {NightReader.new('spec_braille.txt', 'spec_original.txt')}
 
   it "exists" do
     expect(nightreader).to be_a(NightReader)
   end
-  
+
+  it "prints a message based on user input" do
+    allow(nightreader).to receive(:input_file).and_return("0.0.\n..0.\n....\n")
+    expect(nightreader.creation_message).to eq("Created 'spec_original.txt' containing 2 characters")
+  end
+
+  it "can return the amount of characters from the created file" do
+    allow(nightreader).to receive(:input_file).and_return("0.0.\n..0.\n....\n")
+    expect(nightreader.character_count).to eq(2)
+  end
 end
