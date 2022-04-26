bf54c88aa22609086970329fc2abb1d3de2777cf
Sage Skaff <skaffsage@gmail.com>
2022-04-26 12:35:57 -0700

    Refactor runner to take user input

--- night_runner.rb
+++ night_runner.rb
@@ -1,5 +1,17 @@
 require "./lib/night_writer"
 require "./lib/night_reader"
 
-NightWriter.new(ARGV[0], ARGV[1]).write_to_braille
-# NightReader.new(ARGV[0], ARGV[1]).write_to_english
+loop do
+  p "enter 'b' to write to braille, 'e' to write to english"
+  user_input = $stdin.gets.chomp().downcase
+  if    user_input == "b"
+    NightWriter.new(ARGV[0], ARGV[1]).write_to_braille
+    break
+  elsif user_input == "e"
+    NightReader.new(ARGV[0], ARGV[1]).write_to_english
+    break
+  else
+    p "please make a valid selection"
+    sleep(1)
+  end
+end
